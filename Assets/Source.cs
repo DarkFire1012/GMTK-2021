7e39bbf97e1a3ac5e220500a60ee0b2e639485f4
DarkFire1012 <tiongwunlim@gmail.com>
2021-06-13 18:18:19 +0800

    Added Interactables
    Incomplete Actions

---
+++ Assets/Source.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Source : MonoBehaviour
+{
+    [Header("Object Properties")]
+    [SerializeField] private bool Anchored = true;
+    [SerializeField] private bool Stasis = false;
+    [SerializeField] private bool Collidable = true;
+    [SerializeField] private bool Bouncy = false;
+    [SerializeField] private float BounceMultiplier = 1.5f;
+    [SerializeField] private bool Sticky = false;
+    [SerializeField] private bool Indestructable = false;
+    [SerializeField] private bool Flamable = false;
+    [SerializeField] private bool Gravity = false;
+    [SerializeField] private float GravitySpeed = 1;
+
+
+
+
+
+
+}
